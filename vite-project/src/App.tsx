f61c32e7fe3926f8227ba86f86bc029233cd1f6c
JB Moya <jbvhert2002@gmail.com>
2023-08-20 13:18:24 +0800

    hehe

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,54 +4,96 @@ import NavBar from "./components/Navbar";
 
 
 function App() {
-  const [browserWidth, setBrowserWidth] = useState(window.innerWidth);
+  const [isSideBarMenuButtonClicked, setSidebarMenuButtonClicked] = useState(false);
+  const [isClickedOnSmallWindow, setClickedOnSmallWindow ] = useState(false);
 
   const handleResize = () => {
-    setBrowserWidth(window.innerWidth);
+    const sidebarMenu = document.getElementById("sidebarMenu");
+    const container = document.getElementById("container");
+    const newBrowserWidth = window.innerWidth;
 
-    console.log("handleResize in window: " + window.innerWidth);
+    if (!sidebarMenu || !container) {
+      console.log("sidebarMenu or container is null");
+      return;
+    }
 
-    console.log("handleResize: " + browserWidth);
-    if (browserWidth <= 768) {
-      console.log("toggleSidebar: " + browserWidth);
-      const sidebarMenu = document.getElementById("sidebarMenu");
+    const isWindowSmall = newBrowserWidth < 768;
 
-      if (!sidebarMenu) {
-        console.error("sidebarMenu is null");
+    if (isSideBarMenuButtonClicked) {
+      console.log("isSideBarMenuButtonClicked is true");
+
+      if (
+        (isClickedOnSmallWindow && !isWindowSmall) ||
+        (!isClickedOnSmallWindow && isWindowSmall)
+      ) {
+        setSidebarMenuButtonClicked(false);
+        console.log(
+          "isClickedOnSmallWindow is " +
+            (isClickedOnSmallWindow ? "true" : "false")
+        );
+      }
+      return;
+    }
+
+
+    if (!isWindowSmall) {
+      sidebarMenu.classList.remove("hidden");
+      container.classList.remove("expanded");
+      setSidebarMenuButtonClicked(false);
+    } else {
+      if (
+        sidebarMenu.classList.contains("hidden") &&
+        container.classList.contains("expanded")
+      ) {
+        console.log("sidebarMenu is hidden and container is expanded");
         return;
       }
 
-      if (sidebarMenu.classList.contains("hidden")) {
-        sidebarMenu.classList.remove("hidden");
-      }
-      else {
-        sidebarMenu.classList.add("hidden");
-      }
-    }
-    else {
-      console.log("not toggleSidebar");
+      console.log("sidebarMenu is not hidden and container is not expanded");
+      sidebarMenu.classList.add("hidden");
+      container.classList.add("expanded");
+      setSidebarMenuButtonClicked(false);
     }
   };
 
-  // make 4 arbitrary function
-  const toggleSidebar = () = {};
+  const toggleSidebar = () => {
+    const sidebarMenu = document.getElementById("sidebarMenu");
+    const container = document.getElementById("container");
+
+    if (!sidebarMenu) {
+      console.log("sidebarMenu is null");
+      return;
+    }
+
+    sidebarMenu.classList.toggle("hidden");
     
+    if (window.innerWidth >= 768) {
+      container?.classList.toggle("expanded");
+    }
+
+    setSidebarMenuButtonClicked(true)
+
+    const newBrowserWidth = window.innerWidth;
+    newBrowserWidth <= 768 ? setClickedOnSmallWindow(true) : setClickedOnSmallWindow(false);
+  };
+  
   const buttonActions = {
     "toggleSidebar": toggleSidebar,
   };
-
+  
   useEffect(() => {
+    handleResize();
     window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isClickedOnSmallWindow, isSideBarMenuButtonClicked]);
   
   return (
     <>
       <NavBar functions={buttonActions} />
+
       <div id="sidebarMenu">
+        {/* <p>sidebar state: {isWindowSmall.toString()}</p> */}
+
         <a href="#" className="logo">
           <span className="logo-text">Logo</span>
         </a>
@@ -63,7 +105,9 @@ function App() {
         </a>
       </div>
 
-      <div className="container"></div>
+      <div id="container">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, a quod aliquid voluptates, quia fuga laborum eligendi animi soluta quis maxime. Quisquam, officiis similique non deserunt reprehenderit nihil harum delectus temporibus quas veniam. Repellat officiis unde facilis veniam, quaerat molestias pariatur, ducimus ad, quas ea nihil ipsam quos ratione? Nam vitae expedita debitis at suscipit dolorem quasi officia numquam voluptates doloribus delectus similique minima facilis sint porro vero cumque iure adipisci facere nisi, ea accusamus. Cumque beatae porro iste quidem tempora hic quisquam fugiat nisi non impedit odit ipsum, ipsam ab itaque. Veniam recusandae fugit repudiandae dignissimos nam, dolorum consequuntur.</p>
+      </div>
     </>
   );
 }
--- vite-project/src/components/Navbar.tsx
+++ vite-project/src/components/Navbar.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import Button from "./Button";
 
 import {
-  AiOutlinePlus,
   AiFillHome,
   AiOutlineMenu,
-  AiFillStar,
 } from "react-icons/Ai";
 
 interface NavBarProps {
@@ -17,32 +15,12 @@ interface NavBarProps {
 function NavBar({ functions }: NavBarProps) {
   return (
     <div className="navbar">
-      <div>
         <Button
           icon={<AiOutlineMenu />}
           onClick={functions.toggleSidebar}
         ></Button>
         <Button icon={<AiFillHome />}></Button>
         <input type="text" placeholder="Search" />
-      </div>
-
-      <div>
-        <Button
-          icon={<AiFillStar />}
-          text={<span> Upgrade to Pro </span>}
-          extraClass="with-text"
-        ></Button>
-        <Button icon={<AiOutlinePlus />}></Button>
-        <Button
-          icon={
-            <img
-              src="src/assets/profile_picture.png"
-              alt="Icon"
-              className="profile_picture"
-            />
-          }
-        ></Button>
-      </div>
     </div>
   );
 }
