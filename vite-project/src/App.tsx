6f29a115f0091aee0c00fa4db322ca124fd206d5
JB Moya <jbvhert2002@gmail.com>
2023-08-20 15:04:20 +0800

    fa

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "./components/Navbar";
 
-
 function App() {
-  const [isSideBarMenuButtonClicked, setSidebarMenuButtonClicked] = useState(false);
-  const [isClickedOnSmallWindow, setClickedOnSmallWindow ] = useState(false);
+  const [isSideBarMenuButtonClicked, setSidebarMenuButtonClicked] =
+    useState(false);
+  const [isClickedOnSmallWindow, setClickedOnSmallWindow] = useState(false);
+  const isWindowSmall = window.innerWidth <= 768;
+
+  const sidebarMenuClassName =
+    isSideBarMenuButtonClicked || (isWindowSmall && isClickedOnSmallWindow)
+      ? "hidden"
+      : "";
+  const containerClassName =
+    (isSideBarMenuButtonClicked && !isWindowSmall) ||
+    (!isClickedOnSmallWindow && isWindowSmall)
+      ? "expanded"
+      : "";
 
   const handleResize = () => {
     const sidebarMenu = document.getElementById("sidebarMenu");
@@ -34,7 +45,6 @@ function App() {
       }
       return;
     }
-
 
     if (!isWindowSmall) {
       sidebarMenu.classList.remove("hidden");
@@ -66,27 +76,29 @@ function App() {
     }
 
     sidebarMenu.classList.toggle("hidden");
-    
+
     if (window.innerWidth >= 768) {
       container?.classList.toggle("expanded");
     }
 
-    setSidebarMenuButtonClicked(true)
+    setSidebarMenuButtonClicked(true);
 
     const newBrowserWidth = window.innerWidth;
-    newBrowserWidth <= 768 ? setClickedOnSmallWindow(true) : setClickedOnSmallWindow(false);
+    newBrowserWidth <= 768
+      ? setClickedOnSmallWindow(true)
+      : setClickedOnSmallWindow(false);
   };
-  
+
   const buttonActions = {
-    "toggleSidebar": toggleSidebar,
+    toggleSidebar: toggleSidebar,
   };
-  
+
   useEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, [isClickedOnSmallWindow, isSideBarMenuButtonClicked]);
-  
+
   return (
     <>
       <NavBar functions={buttonActions} />
@@ -106,7 +118,19 @@ function App() {
       </div>
 
       <div id="container">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, a quod aliquid voluptates, quia fuga laborum eligendi animi soluta quis maxime. Quisquam, officiis similique non deserunt reprehenderit nihil harum delectus temporibus quas veniam. Repellat officiis unde facilis veniam, quaerat molestias pariatur, ducimus ad, quas ea nihil ipsam quos ratione? Nam vitae expedita debitis at suscipit dolorem quasi officia numquam voluptates doloribus delectus similique minima facilis sint porro vero cumque iure adipisci facere nisi, ea accusamus. Cumque beatae porro iste quidem tempora hic quisquam fugiat nisi non impedit odit ipsum, ipsam ab itaque. Veniam recusandae fugit repudiandae dignissimos nam, dolorum consequuntur.</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, a
+          quod aliquid voluptates, quia fuga laborum eligendi animi soluta quis
+          maxime. Quisquam, officiis similique non deserunt reprehenderit nihil
+          harum delectus temporibus quas veniam. Repellat officiis unde facilis
+          veniam, quaerat molestias pariatur, ducimus ad, quas ea nihil ipsam
+          quos ratione? Nam vitae expedita debitis at suscipit dolorem quasi
+          officia numquam voluptates doloribus delectus similique minima facilis
+          sint porro vero cumque iure adipisci facere nisi, ea accusamus. Cumque
+          beatae porro iste quidem tempora hic quisquam fugiat nisi non impedit
+          odit ipsum, ipsam ab itaque. Veniam recusandae fugit repudiandae
+          dignissimos nam, dolorum consequuntur.
+        </p>
       </div>
     </>
   );
