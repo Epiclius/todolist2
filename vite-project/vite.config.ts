c2b18dbf5d12c719378e3ebb752bb099ab9e138c
JB Moya <jbvhert2002@gmail.com>
2023-09-04 19:45:05 +0800

    deployment

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"{todolist2}"
   plugins: [react()],
 })
+
+// Path: vite-project/src/App.tsx
